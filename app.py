a38418c23d9443a1f286b42a2fa5fc601d035b0f
Isman Kurniawan <isman.krn@gmail.com>
2020-09-26 09:46:04 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas
 from data_prep import *
 from model_pred_sup import *
 from model_pred_uns import *
-import csv
 import shutil
 
 app = Flask(__name__)
@@ -14,7 +13,7 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/",methods=["GET", "POST"])
 def home():
-    target = os.path.join(APP_ROOT, 'uploads/')
+
     if request.method=="POST":
         try:
             getfile = request.files['file_ord']
@@ -26,12 +25,12 @@ def home():
         else:
             # save file
             file_ord = request.files["file_ord"]
-            destination = "/".join([target,file_ord.filename])
-            file_ord.save(destination)
+            # destination = "/".join([target,file_ord.filename])
+            file_ord.save("./uploads/{}".format(file_ord.filename))
 
             file_ord_prod = request.files["file_ord_prod"]
-            destination = "/".join([target,file_ord_prod.filename])
-            file_ord_prod.save(destination)
+            # destination = "/".join([target,file_ord_prod.filename])
+            file_ord_prod.save("./uploads/{}".format(file_ord_prod.filename))
 
             # data prep
             prep_data(file_ord.filename, file_ord_prod.filename)
@@ -46,112 +45,6 @@ def home():
 def download_file():
     filename = "outputs.zip"
     return send_from_directory(os.path.join(APP_ROOT), filename=filename, as_attachment=True)
-
-# @app.route("/",methods=["GET", "POST"])
-# def build():
-#     target = os.path.join(APP_ROOT, 'uploads/')
-#
-#     if request.method=="POST":
-#         gen=gen=request.form['generate']
-#         if gen=="0" :
-#             try:
-#                 getfile = request.files['file']
-#             except:
-#                 return render_template("build.html",selected="0", message="",output="-")
-#
-#             if request.files['file'].filename == '' :
-#                 return render_template("build.html",message="Please choose CSV file")
-#             else:
-#                 file = request.files["file"]
-#                 destination = "/".join([target,file.filename])
-#                 file.save(destination)
-#                 maxModel = count_clmn(destination)
-#                 return render_template("build.html",selected="1", pass_name=destination, maxmodel=maxModel, file_name=file.filename)
-#         elif gen=="1":
-#             # Generate model
-#             destination = request.form['destination']
-#             csv_destination = destination.replace(".csv","")
-#             bestModel = request.form['bestModel']
-#             try:
-#                 qsar_mlr.qsar_web(csv_destination,1,bestModel)
-#                 output = readoverview()
-#                 for i in range(int(bestModel)):
-#                     qsar_mlr.qsar_web(csv_destination,2,'model_'+str(i+1))
-#                     csv_pred = "/".join([APP_ROOT,'model_'+str(i+1)+'_pred'])
-#                     createPlot.create(csv_pred,i)
-#                 plotsrc = "/".join([APP_ROOT,'plot_'])
-#                 # return plotsrc
-#                 return render_template("build.html",message="Success",selected="0", output=output, n_model=int(bestModel))
-#             except:
-#                 return render_template("build.html",message="error",selected="0")
-#     return render_template("build.html",selected="0", message="",output="-")
-#
-# @app.route("/predict",methods=["GET", "POST"])
-# def prediction():
-#     target = os.path.join(APP_ROOT, 'uploads/')
-#
-#     if request.method=="POST":
-#         try:
-#             request.files['fileCsv']
-#         except:
-#             return render_template("predict.html",messaeg="-")
-#         if request.files['fileCsv'].filename == '' :
-#                 return render_template("predict.html",message="Please choose CSV file")
-#         else:
-#             fileCsv = request.files["fileCsv"]
-#             fileModel = request.files["fileModel"]
-#             csv_destination = "/".join([target,fileCsv.filename])
-#             modelDestination = "/".join([target,fileModel.filename])
-#             fileCsv.save(csv_destination)
-#             fileModel.save(modelDestination)
-#             csv_destination = csv_destination.replace(".csv","")
-#             model_Destination = str(fileModel.filename).replace(".p","")
-#             try:
-#                 qsar_mlr.qsar_web(csv_destination,2,model_Destination)
-#                 resultname = (fileModel.filename).replace(".p","_pred.csv")
-#                 head = readresulthead(resultname)
-#                 return render_template("predict.html",message="Success",name=resultname,head=head)
-#             except:
-#                 return render_template("predict.html",message="error")
-#     return render_template("predict.html",messaeg="-")
-#
-# @app.route("/methods",methods=["GET", "POST"])
-# def methods():
-#     return render_template("methods.html",messaeg="-")
-#
-# @app.route('/build/<string:filename>')
-# def download_filse(filename):
-#     # return folder
-#     # try:
-#     #     response = send_from_directory(os.path.join(APP_ROOT),
-#     #                                    filename=filename)
-#     #     response.cache_control.max_age = 60  # e.g. 1 minute
-#     #     return response
-#
-#     # except:
-#     #     return str("asd")
-#     return send_from_directory(os.path.join(APP_ROOT),
-#                                filename=filename, as_attachment=True)
-#
-# def readoverview():
-#     with open("output.txt", "r") as input:
-#         overview = input.read().split("\n\n\n")
-#     return overview
-#
-# def readresulthead(name):
-#     df = pandas.read_csv(name)
-#     head = df.head().round(3)
-#     actual = head['actual'].tolist()
-#     prediction = head['prediction'].tolist()
-#     residual = head['residual'].tolist()
-#     data = [actual,prediction,residual]
-#     return(data)
-#
-# def count_clmn(filename):
-#     with open(filename) as csvfile:
-#         readCSV = csv.reader(csvfile, delimiter=',')
-#         for row in readCSV:
-#             return(len(row))
 
 @app.after_request
 def add_header(r):
